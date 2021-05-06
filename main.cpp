e4760ea13dbf4b909646be3662818421c898dcdd
Dmitry Vedenko <vedenko@gmail.com>
2021-05-06 18:32:47 +0300

    Make the benchmark to run 4 iterations for every variant

--- main.cpp
+++ main.cpp
@@ -78,63 +78,6 @@ BENCHMARK (BM_Write)->ArgsProduct ({
     { 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15, 1 << 16 },
     // Cache size
     { 128, 256, 512, 1024 },
-})->MinTime(2);
+})->Iterations(4)->MeasureProcessCPUTime ()->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN ();
-
-/*
-int main (int argc, char** argv)
-{
-    gflags::ParseCommandLineFlags (&argc, &argv, true);
-
-    fmt::print (
-        "Method: {}, Compression: {}, Page size: {}, Cache size: {}\n", 
-        FLAGS_method, 
-        FLAGS_compression, 
-        (1 << FLAGS_page_size_exp), 
-        FLAGS_cache_size
-    );
-
-    std::vector<Block> blocks = Generate (GetMethod (), FLAGS_blocks_count);
-
-    SQLiteHandle handle;
-
-    if (handle.DB == nullptr)
-        return 1;
-
-    if (!handle.SetupDB ())
-        return 2;
-
-    DBStatement writeStatement(handle.DB, R"=(INSERT INTO
-        sampleblocks (blockid, sampleformat,summin, summax, sumrms, summary256, summary64k,samples)
-        VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8))="
-    );
-
-    if (writeStatement.mStmt == nullptr)
-    {
-        handle.logError ();
-        return 3;
-    }
-
-    const std::chrono::high_resolution_clock::time_point startWrite =
-            std::chrono::high_resolution_clock::now ();
-
-    for (const Block& blk : blocks)
-    {
-        if (!WriteBlock (writeStatement, blk, GetCompressionMethod()))
-        {
-            handle.logError ();
-            return 4;
-        }
-    }
-
-    const std::chrono::high_resolution_clock::time_point endWrite =
-            std::chrono::high_resolution_clock::now ();
-
-    const double writeDuration = std::chrono::duration<double> (endWrite - startWrite).count();
-
-    fmt::print ("Write: {} s, {} block/s\n", writeDuration, FLAGS_blocks_count / writeDuration);
-
-    return 0;
-}
-*/
